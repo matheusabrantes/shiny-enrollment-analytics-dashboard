1561a4702f7bcfcd7779a0ce5b10fbb99c936690
Matheus Abrantes <abrantes_matheus@icloud.com>
2026-01-27 20:06:33 -0300

    Adjust funnel metrics block vertical position

--- modules/components_charts.py
+++ modules/components_charts.py
@@ -105,7 +105,7 @@ def create_funnel_chart(
     annotations = [
         dict(
             x=0.68,
-            y=0.5,
+            y=0.4,
             text="<br>".join(metrics_lines),
             showarrow=False,
             align='left',
